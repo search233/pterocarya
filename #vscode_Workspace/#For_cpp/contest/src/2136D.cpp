654c544176e46af23251ab512e697b5670b0cc79
yrk_arch <yangzw_erik@foxmail.com>
2025-10-16 11:59:58 +0800

    update

---
+++ #vscode_Workspace/#For_cpp/contest/src/2136D.cpp
@@ -0,0 +1,101 @@
+//https://codeforces.com/problemset/problem/2136/D
+
+#include <bits/stdc++.h>
+#define pb push_back
+
+using namespace std;
+using ll = long long;
+using arr2 = array<int, 2>;
+using arr3 = array<int, 3>;
+const int N = (int)2e5 + 9;
+const int M = (int)1e5 + 9;
+const int mod =  998244353;
+const ll INF = LLONG_MAX;
+const double PI = acos(-1.0);
+
+
+void solve() {
+    int n; cin >> n;
+
+    ll max1 = -LLONG_MAX;
+    ll min2 = LLONG_MAX;
+
+    for (int i=0 ; i<n ; ++i) {
+        ll xi, yi;
+        cin >> xi >> yi;
+
+        max1 = max(max1, xi + yi);
+        min2 = min(min2, xi - yi);
+    }
+
+    //num1 : 4k + x + y - min1
+    //num2 : 4k + y - x + min2
+    ll num1, num2;
+
+    cout << "? U 1000000000" << endl;
+    cin >> num1;
+
+    cout << "? U 1000000000" << endl;
+    cin >> num1;
+
+    cout << "? R 1000000000" << endl;
+    cin >> num1;
+
+    cout << "? R 1000000000" << endl;
+    cin >> num1;
+
+    cout << "? L 1000000000" << endl;
+    cin >> num2;
+
+    cout << "? L 1000000000" << endl;
+    cin >> num2;
+
+    cout << "? L 1000000000" << endl;
+    cin >> num2;
+
+    cout << "? L 1000000000" << endl;
+    cin >> num2;
+
+    ll Y = (num1 + num2 - min2 + max1 - 8000000000) / 2;
+    ll X = 4000000000 + Y + min2 - num2;
+
+    cout << "! " << X << ' ' << Y << endl;
+} 
+
+int main() {
+    
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int _ = 1;
+    cin >> _;
+
+    while (_--) {
+        solve();
+        // cout << "-----------\n
+    }
+
+    return 0;
+}
+/*
+┌───┐   ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┐
+│Esc│   │ F1│ F2│ F3│ F4│ │ F5│ F6│ F7│ F8│ │ F9│F10│F11│F12│ │P/S│S L│P/B│
+└───┘   └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┘
+┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐ ┌───┬───┬───┐
+│~ `│! 1│@ 2│# 3│$ 4│% 5│^ 6│& 7│* 8│( 9│) 0│_ -│+ =│ BacSp │ │Ins│Hom│PUp│
+├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤ ├───┼───┼───┤
+│ Tab │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │{ [│} ]│ | \ │ │Del│End│PDn│
+├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤ └───┴───┴───┘
+│ Caps │ A │ S │ D │ F │ G │ H │ J │ K │ L │: ;│" '│ Enter  │              
+├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────────┤     ┌───┐    
+│ Shift  │ Z │ X │ C │ V │ B │ N │ M │< ,│> .│? /│  Shift   │     │ ↑ │    
+├─────┬──┴─┬─┴──┬┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤ ┌───┼───┼───┐
+│Ctrl │Win │Alt │         Space         │Alt │ Fn │Menu│Ctrl│ │ ← │ ↓ │ → │
+└─────┴────┴────┴───────────────────────┴────┴────┴────┴────┘ └───┴───┴───┘
+
+
+  /\_/\
+ (= ._.)
+ / >  \>
+
+*/
