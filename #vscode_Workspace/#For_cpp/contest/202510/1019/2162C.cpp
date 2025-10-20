5dfdd7b4d57d35fac3a976c3abe10ded3906ac39
yrk <yangzw_erik@foxmail.com>
2025-10-20 18:56:51 +0800

    update

---
+++ #vscode_Workspace/#For_cpp/contest/202510/1019/2162C.cpp
@@ -0,0 +1,88 @@
+//https://codeforces.com/problemset/problem/2162/C
+
+#include <bits/stdc++.h>
+#define pb push_back
+
+using namespace std;
+using ll = long long;
+using arr2 = array<int, 2>;
+using arr3 = array<int, 3>;
+const int N = (int)2e5 + 9;
+const int M = (int)1e5 + 9;
+const int mod =  998244353;
+const ll INF = LLONG_MAX;
+const double PI = acos(-1.0);
+
+
+void solve() {
+    int a, b;
+    cin >> a >> b;
+
+    auto cal = [&](int num, int& ans) -> int {
+        int cnt = 0;
+        ans = 0;
+
+        while (num) {
+            if (!(num & 1)) 
+                ans += (1ll << cnt);
+
+            num >>= 1;
+            ++cnt;
+        }
+
+        return cnt;
+    };
+
+    int ans1, ans2;
+
+    if (cal(a, ans1) < cal(b, ans2)) {
+        cout << "-1\n";
+    }
+    else {
+        cout << "2\n";
+        cout << ans1 << ' ';
+
+        a ^= ans1;
+
+        cout << (a ^ b) << '\n';
+    }
+
+} 
+
+int main() {
+    
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int _ = 1;
+    cin >> _;
+
+    while (_--) {
+        solve();
+        // cout << "-----------\n
+    }
+
+    return 0;
+}
+/*
+┌───┐   ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┐
+│Esc│   │ F1│ F2│ F3│ F4│ │ F5│ F6│ F7│ F8│ │ F9│F10│F11│F12│ │P/S│S L│P/B│
+└───┘   └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┘
+┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐ ┌───┬───┬───┐
+│~ `│! 1│@ 2│# 3│$ 4│% 5│^ 6│& 7│* 8│( 9│) 0│_ -│+ =│ BacSp │ │Ins│Hom│PUp│
+├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤ ├───┼───┼───┤
+│ Tab │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │{ [│} ]│ | \ │ │Del│End│PDn│
+├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤ └───┴───┴───┘
+│ Caps │ A │ S │ D │ F │ G │ H │ J │ K │ L │: ;│" '│ Enter  │              
+├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────────┤     ┌───┐    
+│ Shift  │ Z │ X │ C │ V │ B │ N │ M │< ,│> .│? /│  Shift   │     │ ↑ │    
+├─────┬──┴─┬─┴──┬┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤ ┌───┼───┼───┐
+│Ctrl │Win │Alt │         Space         │Alt │ Fn │Menu│Ctrl│ │ ← │ ↓ │ → │
+└─────┴────┴────┴───────────────────────┴────┴────┴────┴────┘ └───┴───┴───┘
+
+
+  /\_/\
+ (= ._.)
+ / >  \>
+
+*/
--- #vscode_Workspace/#For_cpp/contest/src/test.cpp
+++ #vscode_Workspace/#For_cpp/contest/src/test.cpp
@@ -1,27 +1,33 @@
 #include <bits/stdc++.h>
-#define int long long
+
 using namespace std;
 using ll = long long;
-using arr2 = array<int, 2>;
-using arr3 = array<int, 3>;
-const int N = (int)2e5 + 9;
-const int M = (int)1e5 + 9;
-const int mod = (int)1e9 + 7;
-const double pi = acos(-1.0);
-
-double a[N], b[2 * N];
 
 void solve() {
-    cout << 11 << '\n';
+    ll n, k; 
+    cin >> n >> k;
+
+    ll sum = 0;
+
+    for (int i=0 ; i<n ; ++i) {
+        ll tmp; cin >> tmp;
+        sum += tmp;
+    }
+
+    cout << sum - n * (n + 1) / 2 << '\n';
 }
 
 signed main() {
+
     ios::sync_with_stdio(false);
     cin.tie(0);
+
     int _ = 1;
     cin >> _;
+
     while (_--) {
         solve();
     }
+
     return 0;
 }
