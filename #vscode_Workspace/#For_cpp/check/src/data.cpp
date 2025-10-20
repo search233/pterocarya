63978791a63284461702313f8acf2f8b29d0fc86
yrk <yangzw_erik@foxmail.com>
2025-10-20 22:36:43 +0800

    change date_maker in check/

--- #vscode_Workspace/#For_cpp/check/src/data.cpp
+++ #vscode_Workspace/#For_cpp/check/src/data.cpp
@@ -2,38 +2,64 @@
 using namespace std;
 using ll = long long;
 
-void solve() {
+const int N = (int)2e4;
 
-    int m = rand() % 6 + 1;// n 从 1 到 100
-    int n = rand() % 6 + 1;// n 从 1 到 100
+// 1. 生成 [min, max) 范围内的随机整数
+int rand_int_half_open(int min, int max)
+{
+    static std::random_device rd;                              // 非确定性种子
+    static std::mt19937 gen(rd());                             // 32 位梅森旋转算法引擎
+    std::uniform_int_distribution<int> dis(min, max - 1);      // 半开区间
+    return dis(gen);
+}
 
-    cout << n << ' ' << m << '\n';
-    for (int i=1 ; i<=n ; ++i) {
-        int num = rand() % 9 + 1;
-        cout << num << " \n"[i == n];
+// 2. 生成 [min, max] 范围内的随机整数
+int rand_int_closed(int min, int max)
+{
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dis(min, max);          // 闭区间
+    return dis(gen);
+}
+
+void solve(int& sumn) {
+
+    int n = rand_int_closed(1, sumn - 1);
+
+    sumn -= n;
+
+    vector<ll> p(n);
+    iota(p.begin(), p.end(), 1); // p = [1, 2, 3, ..., n]
+
+    random_device rd;  // 从硬件获取随机种子
+    mt19937 gen(rd()); // 梅森旋转随机数引擎（高质量随机性）
+
+    shuffle(p.begin(), p.end(), gen);
+
+    ll ans = rand_int_closed(0, 10000000);
+
+    cout << ans << '\n';
+
+    for (int i=0 ; i<n ; ++i) {
+        ll plus = rand_int_closed(0, ans);
+        p[i] += plus;
+        ans -= plus;
     }
-
-    for (int i=1 ; i<=m ; ++i) {
-        int num = rand() % 9 + 1;
-        cout << num << " \n"[i == m];
-    }
-
-
-
-    // cout << (n & 1 ? n + 1 : n) << '\n';
-
 }
 
 int main()
 {
     srand(time(0));
 
-    int t = rand() % 5 + 1; 
+    int t = rand_int_closed(1, 10000);
+
     // int t = 50;
     cout << t << '\n';
 
+    int sumn = N;
+
     while(t--) {
-        solve();
+        solve(sumn);
     }
 
 	return 0;
