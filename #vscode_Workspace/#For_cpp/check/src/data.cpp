9d9970016c6c8f0cc856654ed82d402348f6acd5
Y7000P <2522763377@qq.com>
2025-11-15 16:15:48 +0800

    make data and update

--- #vscode_Workspace/#For_cpp/check/src/data.cpp
+++ #vscode_Workspace/#For_cpp/check/src/data.cpp
@@ -1,59 +1,153 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
 
-const int N = (int)2e4;
+static const int MAX_A = 26;
 
-// 1. 生成 [min, max) 范围内的随机整数
-int rand_int_half_open(int min, int max)
-{
-    static std::random_device rd;                              // 非确定性种子
-    static std::mt19937 gen(rd());                             // 32 位梅森旋转算法引擎
-    std::uniform_int_distribution<int> dis(min, max - 1);      // 半开区间
-    return dis(gen);
-}
-
-// 2. 生成 [min, max] 范围内的随机整数
-int rand_int_closed(int min, int max)
+// 生成 [L, R] 范围内的随机整数
+int rnd(int L, int R)
 {
     static std::random_device rd;
     static std::mt19937 gen(rd());
-    std::uniform_int_distribution<int> dis(min, max);          // 闭区间
+    std::uniform_int_distribution<int> dis(L, R);          // 闭区间
     return dis(gen);
 }
 
-void solve() {
-    int n = rand_int_closed(2, 9);
-    int q = rand_int_closed(1, n);
+int main() {
 
-    cout << n << ' ' << q << '\n';
-    for (int i=0 ; i<n ; ++i) {
-        int x = rand_int_closed(5, 40);
-        int y = rand_int_closed(x, 70);
-        int z = rand_int_closed(y, 1000);
+    int N0 = 10;     // 初始序列长度，可调整
+    int M = 10;      // 操作次数，可调整
+    int MAXLEN = 33 * N0;
 
-        cout << x << ' ' << y << ' ' << z << '\n';
+    vector<int> A(N0);
+    for (int &x : A) x = rnd(1, MAX_A);
+
+    // 保存生成的输入内容（因为后面需要输出）
+    vector<string> input_lines;
+
+    // 第一行
+    {
+        ostringstream out;
+        out << N0 << " " << M;
+        input_lines.push_back(out.str());
     }
 
-    for (int i=0 ; i<q ; ++i) {
-        cout << i + 1 << ' ';
-    }
-}
-
-int main()
-{
-    srand(time(0));
-
-    int t = 1;
-
-    // int t = 50;
-    // cout << t << '\n';
-
-    // int sumn = N;
-
-    while(t--) {
-        solve();
+    // 初始序列
+    {
+        ostringstream out;
+        for (int i = 0; i < N0; i++) {
+            if (i) out << " ";
+            out << A[i];
+        }
+        input_lines.push_back(out.str());
     }
 
-	return 0;
+    // 开始生成操作并执行真实模拟
+    for (int op_id = 1; op_id <= M; op_id++) {
+        int op = rnd(1, 3);
+
+        // 为了尽量不爆长度，我们限制某些操作概率
+        if (A.size() > MAXLEN * 0.8) {
+            // 序列太长时尽量产生 "缩短" 的操作
+            while (op == 2) op = rnd(1, 3); 
+        }
+
+        {
+            ostringstream out;
+            out << op;
+            input_lines.push_back(out.str());
+        }
+
+        int L = A.size();
+
+        if (op == 1) {
+            // 查找替换
+            int L1 = rnd(1, min(5, max(1, L)));  // 限制 L1 保证能匹配
+            vector<int> seq1(L1);
+            for (int &x : seq1) x = rnd(1, MAX_A);
+
+            // 替换序列可能增加长度，但受 MAXLEN 限制
+            int L2 = rnd(1, 8);
+            if (L + (L2 - L1) > MAXLEN) {
+                L2 = L1; // 避免爆长度
+            }
+            vector<int> seq2(L2);
+            for (int &x : seq2) x = rnd(1, MAX_A);
+
+            {
+                ostringstream out;
+                out << L1;
+                for (int x : seq1) out << " " << x;
+                input_lines.push_back(out.str());
+            }
+            {
+                ostringstream out;
+                out << L2;
+                for (int x : seq2) out << " " << x;
+                input_lines.push_back(out.str());
+            }
+
+            // --- 执行模拟 ---
+            bool found = false;
+            for (int i = 0; i + L1 <= L && !found; i++) {
+                bool ok = true;
+                for (int k = 0; k < L1; k++) {
+                    if (A[i + k] != seq1[k]) { ok = false; break; }
+                }
+                if (ok) {
+                    // 替换第一个
+                    vector<int> B;
+                    B.insert(B.end(), A.begin(), A.begin() + i);
+                    B.insert(B.end(), seq2.begin(), seq2.end());
+                    B.insert(B.end(), A.begin() + i + L1, A.end());
+                    A.swap(B);
+                    found = true;
+                }
+            }
+        }
+
+        else if (op == 2) {
+            // 插入平均数
+            vector<int> B;
+            for (int i = 0; i < L; i++) {
+                B.push_back(A[i]);
+                if (i + 1 < L && ((A[i] + A[i+1]) % 2 == 0)) {
+                    int mid = (A[i] + A[i+1]) / 2;
+                    if ((int)B.size() < MAXLEN) {
+                        B.push_back(mid);
+                    }
+                }
+            }
+            A.swap(B);
+        }
+
+        else if (op == 3) {
+            // 翻转
+            int l = rnd(1, max(1, L));
+            int r = rnd(l, max(1, L));
+            {
+                ostringstream out;
+                out << l << " " << r;
+                input_lines.push_back(out.str());
+            }
+
+            // 执行翻转
+            if (L > 0) reverse(A.begin() + (l-1), A.begin() + r);
+        }
+    }
+
+    // 输出最终结果（用于比对正确答案）
+    vector<int> final_ans = A;
+
+    // ---------- 输出整个测试用例 ----------
+    // 输入
+    for (auto &s : input_lines) cout << s << "\n";
+    // 输出
+    // cout << (int)final_ans.size();
+    // for (int i = 0; i < (int)final_ans.size(); i++) {
+    //     if (i) cout << " ";
+    //     cout << final_ans[i];
+    // }
+    // cout << "\n";
+
+    return 0;
 }
--- #vscode_Workspace/#For_cpp/check/src/my_code.cpp
+++ #vscode_Workspace/#For_cpp/check/src/my_code.cpp
@@ -1,105 +1,35 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-using ll = long long;
-
-void solve() {
-    ll n, q;
-    cin >> n >> q;
-    
-    vector<vector<int>> a(n, vector<int>(4, 0));
-
-    int cnt = 0;
-    for (auto& i : a) {
-        cin >> i[1] >> i[2] >> i[3];
-        i[0] = cnt;
-        ++cnt;  
-    }
-
-    auto cmp1 = [&](vector<int> x, vector<int> y) -> bool {
-        return x[1] > y[1];
-    }; 
-
-    auto cmp2 = [&](vector<int> x, vector<int> y) -> bool {
-        return x[2] > y[2];
-    };
-
-    auto cmp3 = [&](vector<int> x, vector<int> y) -> bool {
-        return x[1]*y[3] > y[1]*x[3];
-    };
-
-    auto cmp4 = [&](vector<int> x, vector<int> y) -> bool {
-        return x[2]*y[3] > y[2]*x[3];
-    };
-
-
-    vector<vector<int>> rk(n + 1, vector<int> (2, n));
-
-    sort(a.begin(), a.end(), cmp1);
-    int trk = 1;
-    for (int i=0 ; i<n ; ++i) {
-        if (i && a[i - 1][1] > a[i][1]) {
-            trk = i + 1;
+ 
+int n, m;
+ 
+int main() {
+    cin >> n >> m;
+    basic_string<int> a(n, 0);
+    for (int& x : a) cin >> x;
+    for (int op, l1, l2, l, r; m--;) {
+        cin >> op;
+        if (op == 1) {
+            cin >> l1;
+            basic_string<int> s1(l1, 0);
+            for (int& x : s1) cin >> x;
+            cin >> l2;
+            basic_string<int> s2(l2, 0);
+            for (int& x : s2) cin >> x;
+            int p = a.find(s1);
+            if (p != a.npos) a.replace(p, l1, s2);
         }
-        if (rk[a[i][0]][0] > trk) {
-            rk[a[i][0]][1] = 1;
-            rk[a[i][0]][0] = trk;
+        if (op == 2)
+            for (int i = a.size() - 2; i >= 0; i--)
+                if ((a[i] + a[i + 1]) % 2 == 0)
+                    a.insert(i + 1, {(a[i] + a[i + 1]) / 2});
+        if (op == 3) {
+            cin >> l >> r;
+            for (int i = l - 1, j = r - 1; i < j; i++, j--)
+                swap(a[i], a[j]);
         }
     }
-
-    sort(a.begin(), a.end(), cmp2);
-    trk = 1;
-    for (int i=0 ; i<n ; ++i) {
-        if (i && a[i - 1][2] > a[i][2]) {
-            trk = i + 1;
-        }
-        if (rk[a[i][0]][0] > trk) {
-            rk[a[i][0]][1] = 2;
-            rk[a[i][0]][0] = trk;
-        }
-    }
-
-    sort(a.begin(), a.end(), cmp3);
-    trk = 1;
-    for (int i=0 ; i<n ; ++i) {
-        if (i && a[i - 1][1]*a[i][3] > a[i][1]*a[i - 1][3]) {
-            trk = i + 1;
-        }
-        if (rk[a[i][0]][0] > trk) {
-            rk[a[i][0]][1] = 3;
-            rk[a[i][0]][0] = trk;
-        }
-    }
-
-    sort(a.begin(), a.end(), cmp4);
-    trk = 1;
-    for (int i=0 ; i<n ; ++i) {
-        if (i && a[i - 1][2]*a[i][3] > a[i][2]*a[i - 1][3]) {
-            trk = i + 1;
-        }
-        if (rk[a[i][0]][0] > trk) {
-            rk[a[i][0]][1] = 4;
-            rk[a[i][0]][0] = trk;
-        }
-    }
-    for (int i=0 ; i<q ; ++i) {
-        int p; cin >> p;
-
-        cout << rk[p][0] << ':' << rk[p][1] << " \n"[i == q-1];
-    }
-}
-
-signed main() {
-
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-
-    int _ = 1;
-    // cin >> _;
-
-    while (_--) {
-        solve();
-    }
-
+    for (int i = 0; int x : a)
+        cout << x << " \n"[++i == a.size()];
     return 0;
 }
