16e5cc2f0b28274cf3e3c5c38d0fdadac9cf6e87
yrk <pterocaryz@outlook.com>
2025-12-17 09:59:03 +0800

    update

--- #vscode_Workspace/#For_cpp/contest/src/2169A.cpp
+++ #vscode_Workspace/#For_cpp/contest/src/2169A.cpp
@@ -1,71 +1,46 @@
-//https://codeforces.com/problemset/problem/2169/A
-
 #include <bits/stdc++.h>
-#define __BUFF__ ios::sync_with_stdio(false);cin.tie(0);
-
 using namespace std;
 using ll = long long;
-using arr2 = array<int, 2>;
-using arr3 = array<int, 3>;
-const int N = (int)2e5 + 9;
-const int M = (int)1e5 + 9;
-const int mod =  998244353;
-const ll INF = LLONG_MAX;
-const double PI = acos(-1.0);
 
-void solve() {
-    int n; cin >> n;
-    int a; cin >> a;
-    map<int, int> mp;
-
-    int cnt = 0;
-    for (int i = 0; i < n; ++i) {
-        int num; cin >> num;
-        ++mp[num];
-        if (num < a) ++cnt;
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<vector<int> > vis(8,vector<int>(7,0));
+    for(int i = 1; i <= n; i++) {
+        int a,b;
+        cin >> a >> b;
+        vis[a][b] = 1;
     }
 
-    if (2 * cnt >= n - mp[a]) {
-        cout << a - 1 << '\n';
-    }
-    else {
-        cout << a + 1 << '\n';
-    }
-} 
+    auto dfs = [&](auto self, int cur, int pre) -> int {
+        if(cur==7)
+            return 1;
+        int res = 0;
+        for (int i = 1; i <= 6;i++){
+            if(i!=pre && !vis[cur+1][i]){
+                vis[cur + 1][i] = 1;
+                res += self(self,cur+1,i);
+                vis[cur + 1][i] = 0;
+            }
+        }
+        return res;
+    };
+    cout << dfs(dfs, 0, 0) << endl;
+}
 
-int main() {
-    
-    __BUFF__
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
     int _ = 1;
-    cin >> _;
-
-    while (_--) {
+    // cin >> _;
+    while (_--)
+    {
         solve();
-        // cout << "-----------\n";
     }
 
     return 0;
 }
-/*
-┌───┐   ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┐
-│Esc│   │ F1│ F2│ F3│ F4│ │ F5│ F6│ F7│ F8│ │ F9│F10│F11│F12│ │P/S│S L│P/B│
-└───┘   └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┘
-┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐ ┌───┬───┬───┐
-│~ `│! 1│@ 2│# 3│$ 4│% 5│^ 6│& 7│* 8│( 9│) 0│_ -│+ =│ BacSp │ │Ins│Hom│PUp│
-├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤ ├───┼───┼───┤
-│ Tab │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │{ [│} ]│ | \ │ │Del│End│PDn│
-├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤ └───┴───┴───┘
-│ Caps │ A │ S │ D │ F │ G │ H │ J │ K │ L │: ;│" '│ Enter  │              
-├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────────┤     ┌───┐    
-│ Shift  │ Z │ X │ C │ V │ B │ N │ M │< ,│> .│? /│  Shift   │     │ ↑ │    
-├─────┬──┴─┬─┴──┬┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤ ┌───┼───┼───┐
-│Ctrl │Win │Alt │         Space         │Alt │ Fn │Menu│Ctrl│ │ ← │ ↓ │ → │
-└─────┴────┴────┴───────────────────────┴────┴────┴────┴────┘ └───┴───┴───┘
-
-
-  /\_/\
- (= ._.)
- / >  \>
-
-*/
---
+++ #vscode_Workspace/#For_cpp/contest/src/2169B.cpp
@@ -0,0 +1,83 @@
+//https://codeforces.com/problemset/problem/2169/B
+
+#include <bits/stdc++.h>
+#define __BUFF__ ios::sync_with_stdio(false);cin.tie(0);
+
+using namespace std;
+using ll = long long;
+using arr2 = array<int, 2>;
+using arr3 = array<int, 3>;
+const int N = (int)2e5 + 9;
+const int M = (int)1e5 + 9;
+const int mod =  998244353;
+const ll INF = LLONG_MAX;
+const double PI = acos(-1.0);
+
+void solve() {
+    string s; cin >> s;
+    vector<int> p1, p2;
+
+    s = ' ' + s + ' ';
+    int cnt = 0;
+    for (int i = 1; i < s.length()-1; ++i) {
+        if (s[i] == '<') {
+            p1.push_back(i);
+        } else if (s[i] == '>') {
+            p2.push_back(i);
+        } else {
+            if (s[i - 1] == '>' || s[i - 1] == '*') {
+                cout << "-1\n";
+                return;
+            } else if (s[i + 1] == '<' || s[i + 1] == '*') {
+                cout << "-1\n";
+                return;
+            } else {
+                ++cnt;
+            }
+        }
+    }
+
+    if (!p1.empty() && !p2.empty() && (p1.back() > p2.front())) {
+        cout << "-1\n";
+        return;
+    }
+
+    cout << max(p1.size(), p2.size()) + cnt << '\n';
+} 
+
+int main() {
+    
+    __BUFF__
+
+    int _ = 1;
+    cin >> _;
+
+    while (_--) {
+        solve();
+        // cout << "-----------\n";
+    }
+
+    return 0;
+}
+/*
+┌───┐   ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┐
+│Esc│   │ F1│ F2│ F3│ F4│ │ F5│ F6│ F7│ F8│ │ F9│F10│F11│F12│ │P/S│S L│P/B│
+└───┘   └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┘
+┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐ ┌───┬───┬───┐
+│~ `│! 1│@ 2│# 3│$ 4│% 5│^ 6│& 7│* 8│( 9│) 0│_ -│+ =│ BacSp │ │Ins│Hom│PUp│
+├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤ ├───┼───┼───┤
+│ Tab │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │{ [│} ]│ | \ │ │Del│End│PDn│
+├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤ └───┴───┴───┘
+│ Caps │ A │ S │ D │ F │ G │ H │ J │ K │ L │: ;│" '│ Enter  │              
+├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────────┤     ┌───┐    
+│ Shift  │ Z │ X │ C │ V │ B │ N │ M │< ,│> .│? /│  Shift   │     │ ↑ │    
+├─────┬──┴─┬─┴──┬┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤ ┌───┼───┼───┐
+│Ctrl │Win │Alt │         Space         │Alt │ Fn │Menu│Ctrl│ │ ← │ ↓ │ → │
+└─────┴────┴────┴───────────────────────┴────┴────┴────┴────┘ └───┴───┴───┘
+
+
+  /\_/\
+ (= ._.)
+ / >  \>
+
+*/
