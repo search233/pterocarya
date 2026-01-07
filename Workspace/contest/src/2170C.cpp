d4fabed83854411ec9634315ab5b294a5b99940b
yangzw <pterocaryz@outlook.com>
2026-01-07 21:25:30 +0800

    change file name

---
+++ Workspace/contest/src/2170C.cpp
@@ -0,0 +1,93 @@
+//https://codeforces.com/problemset/problem/2170/C
+
+#include <bits/stdc++.h>
+#define __BUFF__ ios::sync_with_stdio(false);cin.tie(0);
+
+using namespace std;
+using ll = long long;
+using arr2 = array<int, 2>;
+using arr3 = array<int, 3>;
+const int N = (int)2e5 + 9;
+const int M = (int)1e5 + 9;
+const int mod =  998244353;
+const ll INF = LLONG_MAX;
+const double PI = acos(-1.0);
+
+void solve() {
+    ll n, k; cin >> n >> k;
+
+    vector<ll> r(n + 1), q(n + 1);
+
+    for (int i = 1; i <= n; ++i) {
+        cin >> r[i];
+    }
+    
+    for (int i = 1; i <= n; ++i) {
+        cin >> q[i];
+    }
+
+    sort(r.begin() + 1, r.end());
+    sort(q.begin() + 1, q.end());
+
+    int ans = 0;
+
+    auto ck = [&](int len) -> bool {
+        for (int i = 1; i <= len; ++i) {
+            if ((r[i] + 1ll) * (q[len - i + 1] + 1ll) - 1ll > k) {
+                return false;
+            }
+        }
+        return true;
+    };
+
+    ll L = 0, R = n;
+    while (L <= R) {
+        ll mid = (L + R) / 2;
+
+        if (ck(mid)) {
+            L = mid + 1;
+        }
+        else { 
+          R = mid - 1;
+        }
+    }
+
+    cout << R << '\n';
+} 
+
+int main() {
+    
+    __BUFF__
+
+    int _ = 1;
+    cin >> _;
+
+    while (_--) {
+        solve();
+        // cout << "-----------\n";
+    }
+
+    return 0;
+}
+/*
+┌───┐   ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┐
+│Esc│   │ F1│ F2│ F3│ F4│ │ F5│ F6│ F7│ F8│ │ F9│F10│F11│F12│ │P/S│S L│P/B│
+└───┘   └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┘
+┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐ ┌───┬───┬───┐
+│~ `│! 1│@ 2│# 3│$ 4│% 5│^ 6│& 7│* 8│( 9│) 0│_ -│+ =│ BacSp │ │Ins│Hom│PUp│
+├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤ ├───┼───┼───┤
+│ Tab │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │{ [│} ]│ | \ │ │Del│End│PDn│
+├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤ └───┴───┴───┘
+│ Caps │ A │ S │ D │ F │ G │ H │ J │ K │ L │: ;│" '│ Enter  │              
+├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────────┤     ┌───┐    
+│ Shift  │ Z │ X │ C │ V │ B │ N │ M │< ,│> .│? /│  Shift   │     │ ↑ │    
+├─────┬──┴─┬─┴──┬┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤ ┌───┼───┼───┐
+│Ctrl │Win │Alt │         Space         │Alt │ Fn │Menu│Ctrl│ │ ← │ ↓ │ → │
+└─────┴────┴────┴───────────────────────┴────┴────┴────┴────┘ └───┴───┴───┘
+
+
+  /\_/\
+ (= ._.)
+ / >  \>
+
+*/
