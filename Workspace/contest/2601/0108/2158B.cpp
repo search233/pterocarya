6843aa21004cbb369f316e0788f143c48dda46e3
yangzw <pterocaryz@outlook.com>
2026-01-09 00:44:55 +0800

    daily practice

---
+++ Workspace/contest/2601/0108/2158B.cpp
@@ -0,0 +1,91 @@
+// https://codeforces.com/problemset/problem/2158/B
+
+#include <bits/stdc++.h>
+#define __BUFF__                                                               \
+  ios::sync_with_stdio(false);                                                 \
+  cin.tie(0);
+
+using namespace std;
+using ll = long long;
+using arr2 = array<int, 2>;
+using arr3 = array<int, 3>;
+const int N = (int)2e5 + 9;
+const int M = (int)1e5 + 9;
+const int mod = 998244353;
+const ll INF = LLONG_MAX;
+const double PI = acos(-1.0);
+
+void solve() {
+
+	int n; cin >> n;
+
+	map<int, int> mp;
+
+	for (int i = 0; i < n * 2; ++i) {
+		int num; cin >> num;
+		++mp[num];
+	}
+
+	bool f = 0;
+	int ccnt = 0;
+	int ans = 0;
+
+	for (auto& [num, cnt] : mp) {
+		if (cnt & 1) {
+			++ans;
+			f = 1;
+		}
+		else if (cnt % 4) {
+			ans += 2;
+		}
+		else {
+			++ccnt;
+		}
+	}
+
+	if (ccnt % 2 == 0 || f) {
+		ans += ccnt * 2;
+	}
+	else if (ccnt & 1){
+		ans += (ccnt ^ 1) * 2;
+	}
+
+	cout << ans << '\n';
+}
+
+int main() {
+
+	__BUFF__
+
+	int _ = 1;
+	cin >> _;
+
+	while (_--) {
+		solve();
+		// cout << "-----------\n";
+	}
+
+	return 0;
+}
+/*
+┌───┐   ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┐
+│Esc│   │ F1│ F2│ F3│ F4│ │ F5│ F6│ F7│ F8│ │ F9│F10│F11│F12│ │P/S│S L│P/B│
+└───┘   └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┘
+┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐ ┌───┬───┬───┐
+│~ `│! 1│@ 2│# 3│$ 4│% 5│^ 6│& 7│* 8│( 9│) 0│_ -│+ =│ BacSp │ │Ins│Hom│PUp│
+├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤ ├───┼───┼───┤
+│ Tab │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │{ [│} ]│ | \ │ │Del│End│PDn│
+├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤ └───┴───┴───┘
+│ Caps │ A │ S │ D │ F │ G │ H │ J │ K │ L │: ;│" '│ Enter  │
+├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────────┤     ┌───┐
+│ Shift  │ Z │ X │ C │ V │ B │ N │ M │< ,│> .│? /│  Shift   │     │ ↑ │
+├─────┬──┴─┬─┴──┬┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤ ┌───┼───┼───┐
+│Ctrl │Win │Alt │         Space         │Alt │ Fn │Menu│Ctrl│ │ ← │ ↓ │ → │
+└─────┴────┴────┴───────────────────────┴────┴────┴────┴────┘ └───┴───┴───┘
+
+
+  /\_/\
+ (= ._.)
+ / >  \>
+
+*/
