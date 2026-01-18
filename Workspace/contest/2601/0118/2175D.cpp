2d403d950f5ed4447a0d126aa7c1dc3834f59ac6
yangzw <pterocaryz@outlook.com>
2026-01-18 23:50:59 +0800

    daily practice

---
+++ Workspace/contest/2601/0118/2175D.cpp
@@ -0,0 +1,92 @@
+//https://codeforces.com/problemset/problem/2175/D
+
+#include <bits/stdc++.h>
+#define __BUFF__ ios::sync_with_stdio(false);cin.tie(0);
+
+using namespace std;
+using ll = long long;
+using uint = uint32_t;
+using ull = uint64_t;
+using arr2 = array<int, 2>;
+using arr3 = array<int, 3>;
+const double PI = acos(-1.0);
+
+void solve() {
+    int n, k; cin >> n >> k;
+    vector<int> a(n + 1);
+
+    int mx = 0;
+    
+    vector<int> pos(k + 5, -1);
+    pos[0] = 0;
+    
+    for (int i = 1; i <= n; ++i) {
+        cin >> a[i];
+        while (a[i] > mx) {
+            pos[++mx] = i;
+        }
+    }
+
+    int ans = 0;
+    vector<vector<int>> dp(k + 5, vector<int> (k + 5, -1));
+    dp[0][0] = 0;
+
+    // dp[i][j]
+    for (int i = 1; i <= k; ++i) {
+        if (pos[i] == -1) {
+            continue;
+        }
+
+        for (int j = i; j <= k; ++j) {
+            //dp[x][j - i] -> dp[i][j]
+            for (int x = 0; x < i; ++x) {
+                if (dp[x][j - i] == -1 || pos[x] == -1) {
+                    continue;
+                }
+                int num = dp[x][j - i] + x * (pos[i] - pos[x]);
+                dp[i][j] = max(dp[i][j], num);
+            }
+
+            ans = max(ans, dp[i][j] + i * (n - pos[i] + 1));
+        }
+    }
+
+    cout << ans << '\n';
+}  
+
+int main() {
+    
+    __BUFF__
+
+    int _ = 1;
+    cin >> _;
+
+    while (_--) {
+        solve();
+        // cout << "-----------\n";
+    }
+
+    return 0;
+}
+/*
+┌───┐   ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┐
+│Esc│   │ F1│ F2│ F3│ F4│ │ F5│ F6│ F7│ F8│ │ F9│F10│F11│F12│ │P/S│S L│P/B│
+└───┘   └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┘
+┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐ ┌───┬───┬───┐
+│~ `│! 1│@ 2│# 3│$ 4│% 5│^ 6│& 7│* 8│( 9│) 0│_ -│+ =│ BacSp │ │Ins│Hom│PUp│
+├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤ ├───┼───┼───┤
+│ Tab │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │{ [│} ]│ | \ │ │Del│End│PDn│
+├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤ └───┴───┴───┘
+│ Caps │ A │ S │ D │ F │ G │ H │ J │ K │ L │: ;│" '│ Enter  │              
+├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────────┤     ┌───┐    
+│ Shift  │ Z │ X │ C │ V │ B │ N │ M │< ,│> .│? /│  Shift   │     │ ↑ │    
+├─────┬──┴─┬─┴──┬┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤ ┌───┼───┼───┐
+│Ctrl │Win │Alt │         Space         │Alt │ Fn │Menu│Ctrl│ │ ← │ ↓ │ → │
+└─────┴────┴────┴───────────────────────┴────┴────┴────┴────┘ └───┴───┴───┘
+
+
+  /\_/\
+ (= ._.)
+ / >  \>
+
+*/
