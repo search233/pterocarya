2042f88eb272e9d2406ec50da2c8c30a7b97e084
yangzw <pterocaryz@outlook.com>
2026-01-21 23:55:51 +0800

    d p

---
+++ Workspace/contest/2601/0121/2178C.cpp
@@ -0,0 +1,77 @@
+//https://codeforces.com/problemset/problem/2178/C
+
+#include <bits/stdc++.h>
+#define __BUFF__ ios::sync_with_stdio(false);cin.tie(0);
+
+using namespace std;
+using ll = long long;
+using uint = uint32_t;
+using ull = uint64_t;
+using arr2 = array<int, 2>;
+using arr3 = array<int, 3>;
+const double PI = acos(-1.0);
+
+void solve() {
+    int n; cin >> n;
+    vector<ll> sub(n + 5);
+    vector<ll> sub2(n + 5);
+    vector<ll> a(n + 5);
+
+    for (int i = 1; i <= n; ++i) {
+        cin >> a[i];
+    }
+
+    for (int i = 2; i <= n; ++i) {
+        sub[i] += a[i] + sub[i - 1];
+        sub2[i] += abs(a[i]) + sub2[i - 1];
+    }
+
+    ll ans = sub[1] - sub[n];
+    ll sum = 0;
+
+    for (int i = 2; i <= n; ++i) {
+        // cout << ans << '\n';
+        ll num = a[1] + sub2[i - 1] - (sub[n] - sub[i]);
+        ans = max(ans, num);
+        
+    }
+
+    cout << ans << '\n';
+} 
+ 
+int main() {
+    
+    __BUFF__
+
+    int _ = 1;
+    cin >> _;
+
+    while (_--) {
+        solve();
+        // cout << "-----------\n";
+    }
+
+    return 0;
+}
+/*
+┌───┐   ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┐
+│Esc│   │ F1│ F2│ F3│ F4│ │ F5│ F6│ F7│ F8│ │ F9│F10│F11│F12│ │P/S│S L│P/B│
+└───┘   └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┘
+┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐ ┌───┬───┬───┐
+│~ `│! 1│@ 2│# 3│$ 4│% 5│^ 6│& 7│* 8│( 9│) 0│_ -│+ =│ BacSp │ │Ins│Hom│PUp│
+├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤ ├───┼───┼───┤
+│ Tab │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │{ [│} ]│ | \ │ │Del│End│PDn│
+├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤ └───┴───┴───┘
+│ Caps │ A │ S │ D │ F │ G │ H │ J │ K │ L │: ;│" '│ Enter  │              
+├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────────┤     ┌───┐    
+│ Shift  │ Z │ X │ C │ V │ B │ N │ M │< ,│> .│? /│  Shift   │     │ ↑ │    
+├─────┬──┴─┬─┴──┬┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤ ┌───┼───┼───┐
+│Ctrl │Win │Alt │         Space         │Alt │ Fn │Menu│Ctrl│ │ ← │ ↓ │ → │
+└─────┴────┴────┴───────────────────────┴────┴────┴────┴────┘ └───┴───┴───┘
+
+
+  /\_/\
+ (= ._.)
+ / >  \>
+
+*/
---
+++ Workspace/contest/2601/0121/2178D.cpp
@@ -0,0 +1,99 @@
+//https://codeforces.com/problemset/problem/2178/D
+
+#include <bits/stdc++.h>
+#define __BUFF__ ios::sync_with_stdio(false);cin.tie(0);
+
+using namespace std;
+using ll = long long;
+using uint = uint32_t;
+using ull = uint64_t;
+using arr2 = array<int, 2>;
+using arr3 = array<int, 3>;
+const double PI = acos(-1.0);
+
+void solve() {
+    int n, m; cin >> n >> m;
+    vector<arr2> a(n + 1);
+
+    for (int i = 1; i <= n; ++i) {
+        cin >> a[i][0];
+        a[i][1] = i;
+    }
+
+    sort(a.begin() + 1, a.end());
+
+    // for (int i = 1; i <= n; ++i) {
+    //     cout << a[i][0] << " \n"[i == n];
+    // }
+
+    if (m == 0) {
+        ll sum = 0;
+        int p = n;
+        while (p > 1 && sum < a.back()[0]) {
+            --p;
+            sum += a[p][0];
+        }
+
+        if (sum < a.back()[0]) {
+            cout << "-1\n";
+        }
+        else {
+            cout << n - 1 << '\n';
+            for (int i = 1; i < p; ++i) {
+                cout << a[i][1] << ' ' << a[i + 1][1] << '\n';
+            }
+
+            for (int i = p; i < n; ++i) {
+                cout << a[i][1] << ' ' << a[n][1] << '\n';
+            }
+        }
+    }
+    else {
+        if (m > n / 2) {
+            cout << "-1\n";
+        }
+        else {
+            cout << n - m << '\n';
+            for (int i = 1; i <= n - m; ++i) {
+                cout << a[i + m][1] << ' ' << a[i][1] << '\n';
+            }
+        }
+    }
+} 
+
+int main() {
+    
+    __BUFF__
+
+    int _ = 1;
+    cin >> _;
+
+    while (_--) {
+        solve();
+        // cout << "-----------\n";
+    }
+
+    return 0;
+}
+/*
+┌───┐   ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┐
+│Esc│   │ F1│ F2│ F3│ F4│ │ F5│ F6│ F7│ F8│ │ F9│F10│F11│F12│ │P/S│S L│P/B│
+└───┘   └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┘
+┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐ ┌───┬───┬───┐
+│~ `│! 1│@ 2│# 3│$ 4│% 5│^ 6│& 7│* 8│( 9│) 0│_ -│+ =│ BacSp │ │Ins│Hom│PUp│
+├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤ ├───┼───┼───┤
+│ Tab │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │{ [│} ]│ | \ │ │Del│End│PDn│
+├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤ └───┴───┴───┘
+│ Caps │ A │ S │ D │ F │ G │ H │ J │ K │ L │: ;│" '│ Enter  │              
+├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────────┤     ┌───┐    
+│ Shift  │ Z │ X │ C │ V │ B │ N │ M │< ,│> .│? /│  Shift   │     │ ↑ │    
+├─────┬──┴─┬─┴──┬┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤ ┌───┼───┼───┐
+│Ctrl │Win │Alt │         Space         │Alt │ Fn │Menu│Ctrl│ │ ← │ ↓ │ → │
+└─────┴────┴────┴───────────────────────┴────┴────┴────┴────┘ └───┴───┴───┘
+
+
+  /\_/\
+ (= ._.)
+ / >  \>
+
+*/
